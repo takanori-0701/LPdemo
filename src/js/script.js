7e7ed546d9f7b6172a0e58efd407b2272a7af9ac
prog037 <asd0430.lim.empty@gmail.com>
2025-11-01 23:59:26 +0900

    sidebar-right　位置微調整

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,13 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+  // 全ての img に loading="lazy" を付与（既に属性がある場合は上書きしない）
+  document.querySelectorAll('img').forEach(function(img){
+    if (!img.hasAttribute('loading')) {
+      img.setAttribute('loading', 'lazy');
+    }
+  });
+
   var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
